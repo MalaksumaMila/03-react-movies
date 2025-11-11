b33289a589b2cd12c890309fabd0b2b60346e8f1
Mila <malaksumaaa@gmail.com>
2025-11-11 18:23:22 +0100

    add loader

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,8 +1,5 @@
 import css from './ErrorMessage.module.css';
 
-export default function ErrorMessage {
-    return (
-        <p className={css.text}>There was an error, please try again...</p>
-
-    )
+export default function ErrorMessage() {
+  return <p className={css.text}>There was an error, please try again...</p>;
 }
