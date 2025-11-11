dfe9d65dab2ce4e1f4c476a75f72330fb39e896e
Mila <malaksumaaa@gmail.com>
2025-11-11 17:50:45 +0100

    add MovieGrid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
+
 import type { Movie } from '../../types/movie';
 import fetchMovies from '../../services/movieService';
+import SearchBar from '../SearchBar/SearchBar';
+import toast from 'react-hot-toast';
+import MovieGrid from '../MovieGrid/MovieGrid';
 
 function App() {
-  //     {
-  //   params: {
-  //     // твої параметри
-  //   },
-  //   headers: {
-  //     Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-  //   }
-  // }
+  const [topic, setTopic] = useState<string>('');
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const handleSearch = (q: string) => setTopic(q);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      if (!topic.trim()) return;
+
+      setIsError(false);
+      setIsLoading(true);
+      setMovies([]);
+      try {
+        const data = await fetchMovies(topic);
+
+        if (data.length === 0) {
+          toast.error('No movies found for your request');
+        }
+        setMovies(data);
+      } catch {
+        setIsError(true);
+        toast.error('There was an error, please try again...');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadMovies();
+  }, [topic]);
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <p className={css.text}>Loading movies, please wait...</p>}
+      {isError && (
+        <p className={css.text}>There was an error, please try again...</p>
+      )}
+      <MovieGrid />
+    </>
+  );
 }
 
 export default App;
--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,1 +1,32 @@
 import css from './MovieGrid.module.css';
+import type { Movie } from '../../types/movie';
+
+const IMG_BASE = 'https://image.tmdb.org/t/p/w500';
+interface MovieGridProps {
+  onSelect: (movie: Movie) => void;
+  movies: Movie[];
+}
+export default function MovieGrid({ onSelect, movies }: MovieGridProps) {
+  return (
+    <ul className={css.grid}>
+      {movies.map(movie => (
+        <li
+          key={movie.id}
+          className={css.card}
+          onClick={() => onSelect(movie)}
+        ></li>
+      ))}
+      <li>
+        <div className={css.card}>
+          <img
+            className={css.image}
+            src="https://image.tmdb.org/t/p/w500/poster-path"
+            alt={movie.title}
+            loading="lazy"
+          />
+          <h2 className={css.title}>Movie title</h2>
+        </div>
+      </li>
+    </ul>
+  );
+}
