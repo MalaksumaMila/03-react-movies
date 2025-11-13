e7d3fdb474668e5a0c92809aff762e9fc9eccd18
Mila <malaksumaaa@gmail.com>
2025-11-13 19:44:02 +0100

    add toaster

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <div className={css.app}>
-      <Toaster position= />
+      <Toaster position="top-center" />
       <SearchBar onSubmit={handleSearch} />
 
       {isLoading && <Loader />}
