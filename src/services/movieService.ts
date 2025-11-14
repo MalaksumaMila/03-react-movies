d68bc5348a7ff4197ea401d0d5000fc07882fa10
Mila <malaksumaaa@gmail.com>
2025-11-14 16:41:41 +0100

    correctur

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -5,12 +5,16 @@ import toast from 'react-hot-toast';
 const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
 const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
+interface MoviesServiseRespon {
+  results: Movie[];
+}
+
 export default async function fetchMovies(topic: string): Promise<Movie[]> {
   const query = topic.trim();
   if (!query) return [];
 
   try {
-    const response = await axios.get(`${BASE_URL}`, {
+    const response = await axios.get<MoviesServiseRespon>(`${BASE_URL}`, {
       params: {
         query,
         include_adult: false,
@@ -22,9 +26,13 @@ export default async function fetchMovies(topic: string): Promise<Movie[]> {
       },
     });
 
-    return response.data.results as Movie[];
+    return response.data.results;
   } catch (error) {
     toast.error('There was an error, please try again...');
     return [];
   }
 }
+
+// Запит Axios не використовує явний дженерик аргумент для типізації даних відповіді, що необхідно для безпеки типів.
+// Інтерфейс, що описує структуру даних відповіді (із хоча б results: Movie[]), не визначено локально і не використовується як дженерик аргумент для запиту через Axios.
+// Тип, що повертається з функції, — це проміс<Movie[]>, але оскільки відповідь не типізована, існує ризик небезпеки типів, і використання ствердження типу (as Movie[]) не є ідеальним.
